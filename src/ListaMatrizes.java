7f561dccdac1582c4b6bbf8f5f6ad432ca82883a
Jenifer <jenifergonbrito@gmail.com>
2025-01-06 20:23:56 -0300

    add arquivos de listaEncad, matrizes e add no repositório

---
+++ src/ListaMatrizes.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+public class ListaMatrizes {
+    private Vector<Lista> arrayListas;
+    private int posicaoVetor;
+
+    public ListaMatrizes() {
+        this.arrayListas = new Vector<>();
+    }
+
+    public void inserirMatrizLista(int posicaoVetor){
+        MatrizLista matriz = new MatrizLista();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        MatrizLista matriz1 = new MatrizLista();
+
+    }
+}
