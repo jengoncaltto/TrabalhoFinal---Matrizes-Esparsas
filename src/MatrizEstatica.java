701b0fabd8f283f0b477d6f557533f003a84d56c
Gonçalves <jenifergonbrito@gmail.com>
2025-01-09 17:41:10 -0300

    Update e Add geradorDeMatrizEsparsa MatrizEstatica.java

--- src/MatrizEstatica.java
+++ src/MatrizEstatica.java
@@ -16,7 +16,27 @@ public class MatrizEstatica {
         }
     }
 
-    public int[][] geraAleatorio(){
-        
+   public void gerarMatrizEsparsa(int n) {
+        double grauDeEsparsidade = 0.6;
+        int totalElementos = n*n;
+        int numZeros = (int) (totalElementos * grauDeEsparsidade); // Calcula 60% do total
+        int elementosNaoZero = totalElementos - numZeros;
+        Random random = new Random();
+
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                matriz[i][j] = random.nextInt(9) + 1;
+            }
+        }
+
+        while (numZeros > 0) {
+            int numLinha = random.nextInt(N);
+            int numColuna = random.nextInt(N);
+
+            if (matriz[numLinha][numColuna] != 0) { // Apenas substitui se não for zero
+                matriz[numLinha][numColuna] = 0;
+                numZeros--;
+            }
+        }
     }
 }
