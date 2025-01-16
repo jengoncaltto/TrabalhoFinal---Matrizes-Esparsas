3c852ae5760543f279cef3cf90688c78ab58185b
Luiz Eduardo <68098128+LuizSacramento@users.noreply.github.com>
2025-01-16 20:00:56 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        // roda 10 vezes
+        /*// roda 10 vezes
         long to = System.currentTimeMillis();
         for(int i = 0; i < 10; i++){
             //...
         long t1 = System.currentTimeMillis();
         }
-
         //long tempoTomado = (t1 - t0) / 10;
-
-        MatrizEstatica estatica = new MatrizEstatica(5);
-        estatica.gerarMatrizEsparsa(5);
+        *//*  estatica.gerarMatrizEsparsa(5);
         estatica.exibirMatriz();
+        */
+        MatrizEstatica estatica = new MatrizEstatica(5, 5);
+        estatica.preencherMatriz();
+        estatica.imprimirMatriz();
     }
 }
