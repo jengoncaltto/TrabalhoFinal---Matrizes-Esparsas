993f2a7cbc384ad14c44cdddde3352cdec0c7420
Gonçalves <jenifergonbrito@gmail.com>
2025-01-22 19:10:45 -0300

    Update MainDinamica.java

--- src/MainDinamica.java
+++ src/MainDinamica.java
@@ -3,15 +3,15 @@ public class MainDinamica {
         MatrizDinamica dinamica = new MatrizDinamica(3,3);
         dinamica.gerarMatrizVazia();
         
-        /*dinamica.inserirElemento(1,1,1);
-        dinamica.inserirElemento(1,2,2);
-        dinamica.inserirElemento(1,3,3);*/
+        dinamica.inserirElemento(1,1,0);
+        dinamica.inserirElemento(1,2,1);
+        dinamica.inserirElemento(1,3,0);
         dinamica.inserirElemento(2,1,4);
         dinamica.inserirElemento(2,2,5);
         dinamica.inserirElemento(2,3,6); 
-        /*dinamica.inserirElemento(3,1,7); 
-        dinamica.inserirElemento(3,2,8); 
-        dinamica.inserirElemento(3,3,9); */
+        dinamica.inserirElemento(3,1,0); 
+        dinamica.inserirElemento(3,2,0); 
+        dinamica.inserirElemento(3,3,0); 
         dinamica.imprimirMatriz();
         System.out.println("================");
         System.out.println(dinamica.isMatrizLinha());
